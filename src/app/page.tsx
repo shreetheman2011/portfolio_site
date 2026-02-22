0ac236428a6338615a1f2f49c25cabb35e5b30d6
shreetheman2011 <shree.manickaraja@gmail.com>
2026-02-21 17:49:11 -0800

    try again yt link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
     <main className="min-h-screen bg-black text-white">
       <Navbar />
       <Hero />
-      <a href="https://www.teacheron.com/tutor/cSzB?r=cSzB" target="_blank" style="display: inline-block;"><img src="https://www.teacheron.com/resources/assets/img/badges/viewMyProfile.png" style="width: 160px !important; height: 68px !important"></a>
     </main>
   );
 }
